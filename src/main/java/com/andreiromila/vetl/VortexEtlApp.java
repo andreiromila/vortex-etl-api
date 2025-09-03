4944022a75b21021698af183a2a18543e5091ce0
Andrei Romila <andrei.romila@gmail.com>
2025-09-03 07:04:43 +0200

    feat: ⚙️ Add core Spring application and local database config

---
+++ src/main/java/com/andreiromila/vetl/VortexEtlApp.java
@@ -0,0 +1,13 @@
+package com.andreiromila.vetl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VortexEtlApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VortexEtlApp.class, args);
+	}
+
+}
---
+++ src/test/java/com/andreiromila/vetl/VortexEtlAppTests.java
@@ -0,0 +1,13 @@
+package com.andreiromila.vetl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VortexEtlAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
